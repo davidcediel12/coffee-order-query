37e12cd730b9d0be8e14011218c72641ffc42518
David Cediel <esdago18@gmail.com>
2025-05-07 21:54:33 +0200

    First commit, start project

---
+++ src/main/java/com/cordilleracoffee/orderquery/OrderqueryApplication.java
@@ -0,0 +1,13 @@
+package com.cordilleracoffee.orderquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderqueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cordilleracoffee/orderquery/OrderqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordilleracoffee.orderquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
